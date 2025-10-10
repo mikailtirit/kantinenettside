9c8d4cab1900e011c6b0a47931cd85b7550ffc94
mikailtirit <mikail.tirit@hotmail.com>
2025-10-10 15:02:43 +0200

    la til navbar

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+
+#blok 1: starter flask appen
 from flask import Flask, render_template
 
+#blok 2: lager flask appen for kantinen
 app = Flask (__name__)
+
+
+@app.route('/') #forsiden
+def index():
+ return render_template("index.html")
+
+
 
 
 
