cc85dab6ad0c9af4bbca52e7ffef2dbfd14e217a
mikailtirit <mikail.tirit@hotmail.com>
2025-10-20 15:34:13 +0200

    lagt til navbar i base.html og stylet den med css.

--- app.py
+++ app.py
@@ -6,9 +6,25 @@ from flask import Flask, render_template
 app = Flask (__name__)
 
 
-@app.route('/') #forsiden
+@app.route('/') 
 def index():
- return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route("/meny")
+def meny():
+    ukens_meny = [
+        "Mandag: karbonade og poteter",
+        "Tirsdag: Hannas lassagne",
+        "Onsdag: Taco",
+        "Torsdag: Toast",
+        "Fredag: pannekaker"
+]
+    return render_template("meny.html", ukens_meny=ukens_meny)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
 
