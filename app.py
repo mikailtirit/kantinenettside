8378dc4559ca494f8b7d0a197d07196b3232409f
mikailtirit <mikail.tirit@hotmail.com>
2025-10-23 11:27:32 +0200

    laget varer med base.html, app.py og også varer.html (ikke brukt style.css enda)

--- app.py
+++ app.py
@@ -22,6 +22,20 @@ def meny():
 ]
     return render_template("meny.html", ukens_meny=ukens_meny)
 
+
+@app.route("/varer")
+def varer():
+    faste_varer = [
+        {"navn": "Bagett", "pris": "35kr"},
+        {"navn": "Bolle", "pris": "20kr"},
+        {"navn": "Drikke", "pris": "25kr"}
+    ]
+    return render_template("varer.html", faste_varer=faste_varer)
+
+
+  
+  
+  
 if __name__ == "__main__":
     app.run(debug=True)
 
